a98c55250d7f0429ef3ff8afdb4e08b48418ee92
Rushikesh <pitalerushikesh@gmail.com>
2022-02-28 18:52:33 +0530

    final5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import "./user/css/Homepage.css";
 import Router from "./Router";
 import { Provider } from "react-redux";
 import { Store } from "./components/redux/Store";
